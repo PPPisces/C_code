340092e7e3fa3f95af0169852f94877856dd9fdd
PPPisces <ztypisces@gmail.com>
2022-01-07 17:42:13 +0800

    c course

---
+++ Project5 0105 2022/test 0105 2022.c
@@ -0,0 +1,50 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include <math.h>
+int main()
+{
+
+	return 0;
+}
+//int main()
+//{
+//	int n = 0;
+//	while (scanf("%d", &n) != EOF)
+//	{
+//		printf("%d\n", 1 << n); //pow(2,n)
+//	}
+//
+//	return 0;
+//}
+//int main()
+//{
+//	int ch = 0;
+//	while ((ch = getchar()) != EOF)
+//	{
+//		if(	isalpha(ch))
+//		{
+//
+//		}
+//	}
+//
+//	return 0;
+//}
+//从键盘输入任意字符，判断是否是字母包括大小写
+//int main()
+//{
+//	int ch = 0;
+//	while ((ch = getchar()) != EOF)
+//	{
+//		if ((ch >= 'A' && ch <= 'Z') || (ch >= 'a' && ch <= 'z'))
+//		{
+//			printf("YES\n");
+//
+//		}
+//		else
+//		{
+//			printf("NO\n");
+//		}
+//		getchar();
+//	}
+//	return 0;
+//}
